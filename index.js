caaf391e228e7b45968c87431d79a6049700f37d
Isaiah <ijandicos90@gmail.com>
2022-03-09 20:33:05 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+return a - b
+}
+
+function multiply(a, b){
+return a * b
+}
+
+function divide(a, b) {
+return a/b
+}
+
+let n = "8"
+
+function increment(n){
+    return n += 1
+}
+
+function decrement(n){
+    return n -= 1
+}
+
+let digIt = "43.7"
+function makeInt(digIt) {
+    return parseInt(digIt, 10)
+}
+
+function preserveDecimal(digIt) {
+    return parseFloat(digIt)
+}
+
